6a80fa3ef4be03ed58f78ecd892955717d0fa883
Frantisek Janus <frantisek.janus@swisscom.com>
2020-07-28 10:12:37 +0200

    add new code

---
+++ goroutine/channel_close_improper/chan_improper.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	msg := make(chan string)
+	until := time.After(5 * time.Second)
+
+	go send(msg)
+
+	for {
+		select {
+		case m := <-msg:
+			fmt.Println(m)
+		case <-until:
+			close(msg) // this is wrong: close() should be used only by sender
+			time.Sleep(500 * time.Millisecond)
+			return
+		}
+	}
+}
+
+func send(ch chan string) {
+	for {
+		ch <- "hello"
+		time.Sleep(500 * time.Millisecond)
+	}
+}
