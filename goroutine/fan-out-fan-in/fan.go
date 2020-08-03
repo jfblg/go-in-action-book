116f3ac15712c77efc9cdfb4fc8ee9eab30c53cb
Frantisek Janus <frantisek.janus@swisscom.com>
2020-08-03 17:27:26 +0200

    add take function

--- goroutine/fan-out-fan-in/fan.go
+++ goroutine/fan-out-fan-in/fan.go
@@ -46,6 +46,25 @@ func main() {
 		return valueStreamInt
 	}
 
+	take := func(
+		done <-chan interface{},
+		valueStream <-chan interface{},
+		num int,
+	) <-chan interface{} {
+		takeStream := make(chan interface{})
+		go func() {
+			defer close(takeStream)
+			for i := 0; i < num; i++ {
+				select {
+				case <-done:
+					return
+				case takeStream <- <-valueStream:
+				}
+			}
+		}()
+		return takeStream
+	}
+
 	fanIn := func(
 		done <-chan interface{},
 		channels ...<-chan interface{},
